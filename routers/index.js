8056acaab8c60d94acece83c02d1e3baef0cf6a4
unknown <dan10kiy@gmail.com>
2022-01-19 09:48:16 +0700

    first commit

--- routers/index.js
+++ routers/index.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const authorize = require('../middleware/authorize');
+// const authorize = require('../middleware/authorize');
 
 router.use(require("./pengurus"));
-router.use(authorize, require("./shift"));
-router.use(authorize, require("./wilayah"));
-router.use(authorize, require("./hari"));
-router.use(authorize, require("./reservasi"));
-router.use(authorize, require("./wali"));
-router.use(authorize, require("./santri"));
-router.use(authorize, require("./reservasi_wali"));
-router.use(authorize, require("./reservasi_santri"));
-router.use(authorize, require("./pertemuan"));
-router.use(authorize, require("./informasi"));
+router.use(require("./shift"));
+router.use(require("./wilayah"));
+router.use(require("./hari"));
+router.use(require("./reservasi"));
+router.use(require("./wali"));
+router.use(require("./santri"));
+router.use(require("./reservasi_wali"));
+router.use(require("./reservasi_santri"));
+router.use(require("./pertemuan"));
+router.use(require("./informasi"));
 
 module.exports = router;
