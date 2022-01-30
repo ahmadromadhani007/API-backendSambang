437644603d4442b382766f164afeb1578d763722
sunak27 <alfadsabilhaq@gmail.com>
2022-01-31 01:02:18 +0700

    no_mahrom

--- routers/reservasi.js
+++ routers/reservasi.js
@@ -5,6 +5,7 @@ const router = express.Router();
 router.get("/reservasi", reservasi.tampil);
 router.get("/reservasiId", reservasi.id);
 router.get("/reservasi/:id", reservasi.cari);
+router.get("/reservasi/mahrom/:no_mahrom", reservasi.mahrom);
 router.get("/reservasi/tanggal/:tgl_kunjungan", reservasi.tgl_kunjungan);
 router.post("/reservasi", reservasi.simpan);
 router.put("/reservasi/:id", reservasi.edit);
